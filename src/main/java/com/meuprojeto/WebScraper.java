d2c7e24f470a2279848bd4bf1c7403113df856d1
Caio <caionzxt@gmail.com>
2025-03-28 01:35:12 -0300

    Modificado para baixar apenas links específicos de PDFs

--- src/main/java/com/meuprojeto/WebScraper.java
+++ src/main/java/com/meuprojeto/WebScraper.java
@@ -1,37 +1,37 @@
 package com.meuprojeto;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 import java.io.*;
-import java.net.URL;
 import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class WebScraper {
 
-    private static final String TARGET_URL = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-dasociedade/atualizacao-do-rol-de-procedimentos";
     private static final String DOWNLOAD_FOLDER = "downloads/";
     private static final String ZIP_FILE = "anexos.zip";
 
     public static void main(String[] args) {
         try {
+            // Criar pasta para armazenar os PDFs baixados
             File dir = new File(DOWNLOAD_FOLDER);
             if (!dir.exists()) {
                 dir.mkdirs();
             }
 
-            Document document = Jsoup.connect(TARGET_URL).get();
-            Elements links = document.select("a[href$=.pdf]");
+            // Links específicos para download
+            String[] pdfLinks = {
+                "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf",
+                "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf"
+            };
 
-            for (Element link : links) {
-                String fileUrl = link.absUrl("href");
-                String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
-                downloadFile(fileUrl, DOWNLOAD_FOLDER + fileName);
+            // Baixar os arquivos PDFs especificados
+            for (String pdfUrl : pdfLinks) {
+                String fileName = pdfUrl.substring(pdfUrl.lastIndexOf("/") + 1);
+                downloadFile(pdfUrl, DOWNLOAD_FOLDER + fileName);
             }
 
+            // Compactar os arquivos baixados em um ZIP
             zipFiles(DOWNLOAD_FOLDER, ZIP_FILE);
             System.out.println("Processo concluído. Arquivos compactados em: " + ZIP_FILE);
 
@@ -40,16 +40,17 @@ public class WebScraper {
         }
     }
 
+    // Método para baixar o arquivo
     public static void downloadFile(String fileURL, String savePath) throws IOException {
         System.out.println("Baixando: " + fileURL);
-        
+
         URL url = new URL(fileURL);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
 
         try (InputStream inputStream = connection.getInputStream();
              FileOutputStream outputStream = new FileOutputStream(savePath)) {
-            
+
             byte[] buffer = new byte[4096];
             int bytesRead;
             while ((bytesRead = inputStream.read(buffer)) != -1) {
@@ -59,6 +60,7 @@ public class WebScraper {
         System.out.println("Download concluído: " + savePath);
     }
 
+    // Método para compactar os arquivos em um arquivo ZIP
     public static void zipFiles(String sourceFolder, String zipFilePath) throws IOException {
         File directory = new File(sourceFolder);
         File[] files = directory.listFiles();
