f2d52c0b2b11b90b8ec382b4cd9d91e19affa85e
Caio <caionzxt@gmail.com>
2025-03-28 01:47:21 -0300

    Modificado para baixar e compactar apenas os links específicos de PDFs

--- src/main/java/com/meuprojeto/WebScraper.java
+++ src/main/java/com/meuprojeto/WebScraper.java
@@ -1,13 +1,18 @@
 package com.meuprojeto;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import java.io.*;
-import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.HttpURLConnection;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class WebScraper {
 
+    private static final String TARGET_URL = "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos";
     private static final String DOWNLOAD_FOLDER = "downloads/";
     private static final String ZIP_FILE = "anexos.zip";
 
@@ -19,16 +24,18 @@ public class WebScraper {
                 dir.mkdirs();
             }
 
-            // Links específicos para download
+            // Definir os links dos PDFs específicos
             String[] pdfLinks = {
                 "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf",
                 "https://www.gov.br/ans/pt-br/acesso-a-informacao/participacao-da-sociedade/atualizacao-do-rol-de-procedimentos/Anexo_II_DUT_2021_RN_465.2021_RN628.2025_RN629.2025.pdf"
             };
 
-            // Baixar os arquivos PDFs especificados
-            for (String pdfUrl : pdfLinks) {
-                String fileName = pdfUrl.substring(pdfUrl.lastIndexOf("/") + 1);
-                downloadFile(pdfUrl, DOWNLOAD_FOLDER + fileName);
+            // Baixar os arquivos PDF específicos
+            for (String fileUrl : pdfLinks) {
+                String fileName = fileUrl.substring(fileUrl.lastIndexOf("/") + 1);
+
+                // Baixar o arquivo PDF
+                downloadFile(fileUrl, DOWNLOAD_FOLDER + fileName);
             }
 
             // Compactar os arquivos baixados em um ZIP
@@ -40,17 +47,19 @@ public class WebScraper {
         }
     }
 
-    // Método para baixar o arquivo
+    /**
+     * Método responsável por baixar arquivos da web e salvar localmente.
+     */
     public static void downloadFile(String fileURL, String savePath) throws IOException {
         System.out.println("Baixando: " + fileURL);
-
+        
         URL url = new URL(fileURL);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("GET");
 
         try (InputStream inputStream = connection.getInputStream();
              FileOutputStream outputStream = new FileOutputStream(savePath)) {
-
+            
             byte[] buffer = new byte[4096];
             int bytesRead;
             while ((bytesRead = inputStream.read(buffer)) != -1) {
@@ -60,7 +69,9 @@ public class WebScraper {
         System.out.println("Download concluído: " + savePath);
     }
 
-    // Método para compactar os arquivos em um arquivo ZIP
+    /**
+     * Método responsável por compactar os arquivos baixados em um ZIP.
+     */
     public static void zipFiles(String sourceFolder, String zipFilePath) throws IOException {
         File directory = new File(sourceFolder);
         File[] files = directory.listFiles();
@@ -74,17 +85,19 @@ public class WebScraper {
              ZipOutputStream zos = new ZipOutputStream(fos)) {
 
             for (File file : files) {
-                try (FileInputStream fis = new FileInputStream(file)) {
-                    ZipEntry zipEntry = new ZipEntry(file.getName());
-                    zos.putNextEntry(zipEntry);
+                if (file.isFile()) {  // Verificar se é um arquivo (e não um diretório)
+                    try (FileInputStream fis = new FileInputStream(file)) {
+                        ZipEntry zipEntry = new ZipEntry(file.getName());
+                        zos.putNextEntry(zipEntry);
 
-                    byte[] buffer = new byte[1024];
-                    int length;
-                    while ((length = fis.read(buffer)) > 0) {
-                        zos.write(buffer, 0, length);
+                        byte[] buffer = new byte[1024];
+                        int length;
+                        while ((length = fis.read(buffer)) > 0) {
+                            zos.write(buffer, 0, length);
+                        }
+
+                        zos.closeEntry();
                     }
-
-                    zos.closeEntry();
                 }
             }
         }
